1d08fdf74785ef15a1825329aa5004aeaf4af2be
Austin Nicholas <austin@adenix.io>
2021-10-03 19:03:08 -0400

    Updated unit test to use sub runs

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -2,6 +2,7 @@ package calculator
 
 import (
 	"context"
+	"fmt"
 	"math/rand"
 	"sync"
 	"testing"
@@ -16,7 +17,7 @@ func TestSum(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
 
 	var wg sync.WaitGroup
-	for i := 0; i < 100; i++ {
+	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 
 		var a int32 = -300 + int32(rand.Intn(600))
@@ -25,15 +26,17 @@ func TestSum(t *testing.T) {
 		go func(a, b int32) {
 			defer wg.Done()
 
-			res, err := c.Sum(context.Background(), &calculatorpb.SumRequest{A: a, B: b})
-			if err != nil {
-				t.Fatalf("failed to sum request: %q", err)
-			}
+			t.Run(fmt.Sprintf("sum-random-%d", i), func(t *testing.T) {
+				res, err := c.Sum(context.Background(), &calculatorpb.SumRequest{A: a, B: b})
+				if err != nil {
+					t.Fatalf("failed to sum request: %q", err)
+				}
 
-			expected := a + b
-			if r := res.GetResult(); r != expected {
-				t.Errorf("expected %d but got %d", expected, r)
-			}
+				expected := a + b
+				if r := res.GetResult(); r != expected {
+					t.Errorf("expected %d but got %d", expected, r)
+				}
+			})
 		}(a, b)
 	}
 
